8dea9667734eb4bb2d17019777c831a080d35f95
sanmaysood <99178902+sanmaysood@users.noreply.github.com>
2022-11-26 20:35:35 +0530

    Update date.c

--- Part1/date.c
+++ Part1/date.c
@@ -7,6 +7,7 @@
 
 
 int main(int argc, char ** args){
+
     if(argc == 1){
         char week_day[100];
         char month_name[100];
@@ -16,7 +17,9 @@ int main(int argc, char ** args){
         strftime(month_name, 100, "%b", time);
         printf("%s %s %d %d:%d:%d IST 2022\n", week_day, month_name,time->tm_mday, time->tm_hour, time->tm_min, time->tm_sec);
     }
+
     else if(argc == 2){
+
         if(strcmp(args[1],"-u") == 0){
             char week_day[100];
             char month_name[100];
@@ -26,6 +29,7 @@ int main(int argc, char ** args){
             strftime(month_name, 100, "%b", time);
             printf("%s %s %d %d:%d:%d UTC 2022\n", week_day, month_name,time->tm_mday, time->tm_hour, time->tm_min, time->tm_sec);
         }
+
         else if(strcmp(args[1],"-R") == 0){
             char week_day[100];
             char month_name[100];
@@ -35,12 +39,15 @@ int main(int argc, char ** args){
             strftime(month_name, 100, "%b", time);
             printf("%s, %d %s 2022 %d:%d:%d +0530\n", week_day,time->tm_mday,month_name , time->tm_hour, time->tm_min, time->tm_sec);
         }
+        
         else{
-            perror("Invalid command\n");
+            printf("Invalid command\n");
         }
     }
+
     else{
-        perror("Too many arguments\n");
+        printf("Too many arguments\n");
     }
+
     return 0;
 }
