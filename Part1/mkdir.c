76f92ac4aaae95719d6298e715569fbc34801404
sanmaysood <99178902+sanmaysood@users.noreply.github.com>
2022-11-26 20:34:33 +0530

    Update mkdir.c

--- Part1/mkdir.c
+++ Part1/mkdir.c
@@ -5,38 +5,61 @@
 # include <sys/types.h>
 # include <sys/stat.h>
 
+
 int main(int argc, char ** args){
+
     if(argc == 1){
-        perror("No arguments passed\n");
+        printf("No arguments passed\n");
     }
+
     else if(argc == 2){
         int m = mkdir(args[1], 0777);
+
         if(m == -1){
-            perror("Directory already exists\n");
+            printf("Directory already exists\n");
         }
     }
+
     else if(argc == 3){
+
         if(strcmp(args[1], "-v") == 0){
             int m = mkdir(args[2], 0777);
+
             if(m == -1){
-                perror("Directory already exists\n");
+                printf("Directory already exists\n");
             }
+
             else{
                 printf("Created directory - %s\n", args[2]);
             }
         }
+
         else if(strcmp(args[1], "-p") == 0){
-            int m = mkdir(args[2], 0777);
-            if(m == -1){
-                perror("Directory already exists\n");
+            
+            char * path = args[2];
+            char * current = strtok(path,"/");
+
+            while(current != NULL){
+
+                int m = mkdir(current,0777);
+                if(m == -1){
+                    printf("Directory already exists\n");
+                }
+                
+                chdir(current);
+                current = strtok(NULL,"/");
+                
             }
         }
+
         else{
-            perror("Invalid command\n");
+            printf("Invalid command\n");
         }
     }
+
     else{
-        perror("Too many arguments\n");
+        printf("Too many arguments\n");
     }
+
     return 0;
 }
